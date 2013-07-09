7c462f1054463f3493c3b37fd7db7149389ec721
Yegor Yefremov <yegorslists@googlemail.com>
2013-07-09 09:12:36 -0700

    slcan2d: add UART speed configuration
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>

--- slcan2d.c
+++ slcan2d.c
@@ -78,6 +78,7 @@ void print_usage(char *prg)
 	fprintf(stderr, "         -c         (send close command 'C\\r')\n");
 	fprintf(stderr, "         -f         (read status flags with 'F\\r' to reset error states)\n");
 	fprintf(stderr, "         -s <speed> (set CAN speed 0..8)\n");
+	fprintf(stderr, "         -S <speed> (set UART speed)\n");
 	fprintf(stderr, "         -b <btr>   (set bit time register value)\n");
 	fprintf(stderr, "         -F         (stay in foreground; no daemonize)\n");
 	fprintf(stderr, "         -h         (show this help page)\n");
@@ -115,6 +116,62 @@ static void child_handler (int signum)
 		exit_code = EXIT_SUCCESS;
 		slcan2d_running = 0;
 		break;
+	}
+}
+
+static int look_up_uart_speed(int s)
+{
+	switch (s) {
+	case 9600:
+		return B9600;
+	case 19200:
+		return B19200;
+	case 38400:
+		return B38400;
+	case 57600:
+		return B57600;
+	case 115200:
+		return B115200;
+	case 230400:
+		return B230400;
+	case 460800:
+		return B460800;
+	case 500000:
+		return B500000;
+	case 576000:
+		return B576000;
+	case 921600:
+		return B921600;
+	case 1000000:
+		return B1000000;
+	case 1152000:
+		return B1152000;
+	case 1500000:
+		return B1500000;
+	case 2000000:
+		return B2000000;
+#ifdef B2500000
+	case 2500000:
+		return B2500000;
+#endif
+#ifdef B3000000
+	case 3000000:
+		return B3000000;
+#endif
+#ifdef B3500000
+	case 3500000:
+		return B3500000;
+#endif
+#ifdef B3710000
+	case 3710000
+		return B3710000;
+#endif
+#ifdef B4000000
+	case 4000000:
+		return B4000000;
+#endif
+	default:
+		return B57600;
 	}
 }
 
@@ -251,13 +308,15 @@ int main (int argc, char *argv[])
 	int send_close = 0;
 	int send_read_status_flags = 0;
 	char *speed = NULL;
+	char *uart_speed_str = NULL;
+	long int uart_speed = 0;
 	char *btr = NULL;
 	int run_as_daemon = 1;
 	pid_t parent_pid = 0;
 
 	ttypath[0] = '\0';
 
-	while ((opt = getopt(argc, argv, "ocfs:b:?hF")) != -1) {
+	while ((opt = getopt(argc, argv, "ocfs:S:b:?hF")) != -1) {
 		switch (opt) {
 		case 'o':
 			send_open = 1;
@@ -271,6 +330,13 @@ int main (int argc, char *argv[])
 		case 's':
 			speed = optarg;
 			if (strlen(speed) > 1)
+				print_usage(argv[0]);
+			break;
+		case 'S':
+			uart_speed_str = optarg;
+			errno = 0;
+			uart_speed = strtol(uart_speed_str, NULL, 10);
+			if (errno)
 				print_usage(argv[0]);
 			break;
 		case 'b':
@@ -345,8 +411,8 @@ int main (int argc, char *argv[])
     old_ospeed = cfgetospeed(&tios);
 
     /* Baud Rate */
-    cfsetispeed(&tios, B3000000);
-    cfsetospeed(&tios, B3000000);
+    cfsetispeed(&tios, look_up_uart_speed(uart_speed));
+    cfsetospeed(&tios, look_up_uart_speed(uart_speed));
 
     /* apply changes */
     if(tcsetattr(fd, TCSADRAIN, &tios) < 0) {
